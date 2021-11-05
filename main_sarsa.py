68a965c9d2f02fad44b8c5503d74cd7327cd8b68
Davi Melo <davilobomelo@gmail.com>
2021-11-05 01:37:52 -0300

    sarsa

--- main_sarsa.py
+++ main_sarsa.py
@@ -11,106 +11,76 @@ from sarsa import MOVEMENT, REWARD, Blob, Sarsa, Tables
 
 style.use("ggplot")
 
+
+def moving_average(a, n=10):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+
 MOVE_REWARD = -1
 ENEMY_REWARD = -300
-FOOD_REWARD = 25
+FOOD_REWARD = 100
 
 SIZE = 10
-N_EPISODES = 1000
-N_MAX_STEPS_IN_EPISODE = 100
-
-CONST_EPSILON = 100
-GAMA = 0.9
-LAMBDA = 0.2
-
-SHOW_EVERY = 3000  # how often to play through env visually.
-
-# Em duvida se será que Q table ou V table...
-
-PLAYER_N = 1  # player key in dict
-FOOD_N = 2  # food key in dict
-ENEMY_N = 3  # enemy key in dict
+N_EPISODES = 50_000
+N_MAX_STEPS_IN_EPISODE = 200
 
 q_table = Tables.new_table(SIZE, 4)
-nsa_table = Tables.new_table(SIZE, 4)  # quantidade de ações por estado
+nsa_table = Tables.new_table(SIZE, 4)
 eligibility_traces = Tables.new_table(SIZE, 4)
+ns_table = Tables.new_table(SIZE, 1)
 
-sarsa = Sarsa(0.99, 0.1, 0.1, q_table, nsa_table, eligibility_traces)
+for llambda in np.array([0, 0.2, 0.4, 0.6, 0.8, 1]):
+    print(f'Lambda = {llambda}')
+    sarsa_learning = Sarsa(0.95, llambda, 0.85, q_table, nsa_table,
+                           eligibility_traces, ns_table)
 
-optimalvalues = []
-for episode in tqdm(range(N_EPISODES)):
+    optimalvalues = []
+    for episode in tqdm(range(N_EPISODES)):
 
-    player = Blob(SIZE)
-    food = Blob(SIZE)
-    enemy = Blob(SIZE)
+        player = Blob(SIZE)
+        food = Blob(SIZE)
+        enemy = Blob(SIZE)
 
-    sarsa.new_episode()
+        episode_rewards = 0
 
-    state = (player-food, player-enemy)
-    sarsa.append_episode_state(state)
-    action = sarsa.get_action(state)
-    sarsa.set_nsa_table(state, action)
+        sarsa_learning.new_episode()
 
-    # if episode % SHOW_EVERY == 0:
-    #     print(f"on #{episode}, mean epsilon is WRITE SOMETHING HERE")
-    #     print(f"{SHOW_EVERY} TO BE DECEIDED")
-    #     show = False
-    # else:
-    #     show = False
+        for step in range(N_MAX_STEPS_IN_EPISODE):
 
-    for step in range(N_MAX_STEPS_IN_EPISODE):
+            if player == enemy or player == food:
+                break
 
-        player.action(MOVEMENT(action))
+            episode_state = (player-food, player-enemy)
+            action = sarsa_learning.epsilon_greedy_policy(episode_state)
 
-        reward = 0
-        if player == enemy:
-            reward = ENEMY_REWARD
-        elif player == food:
-            reward = FOOD_REWARD
-        else:
-            reward = MOVE_REWARD
-        sarsa.set_reward(reward)
+            player.action(MOVEMENT(action))
 
-        state = (player-food, player-enemy)
-        sarsa.append_episode_state(state)
+            reward = 0
+            if player == enemy:
+                reward = ENEMY_REWARD
+            elif player == food:
+                reward = FOOD_REWARD
+            else:
+                reward = MOVE_REWARD
 
-        action = sarsa.get_action(state)
-        sarsa.set_nsa_table(state, action)
+            episode_rewards += reward
+            sarsa_learning.update_episode(episode_state, reward, action)
 
-        sarsa.update()
+            if step == 0:
+                continue
 
-        # if False:
-        #     # starts an rbg of our size
-        #     env = np.zeros((SIZE, SIZE, 3), dtype=np.uint8)
-        #     # sets the food location tile to green color
-        #     env[food.x][food.y] = d[FOOD_N]
-        #     # sets the player tile to blue
-        #     env[player.x][player.y] = d[PLAYER_N]
-        #     # sets the enemy location to red
-        #     env[enemy.x][enemy.y] = d[ENEMY_N]
-        #     # reading to rgb. Apparently. Even tho color definitions are bgr. ???
-        #     img = Image.fromarray(env, 'RGB')
-        #     # resizing so we can see our agent in all its glory.
-        #     img = img.resize((300, 300))
-        #     cv2.imshow("image", np.array(img))  # show it!
-        #     # crummy code to hang at the end if we reach abrupt end for good reasons or not.
-        #     if player == enemy or player == food:
-        #         if cv2.waitKey(500) & 0xFF == ord('q'):
-        #             break
-        #     else:
-        #         if cv2.waitKey(1) & 0xFF == ord('q'):
-        #             break
+            sarsa_learning.update_tables()
 
-        # if player == enemy or player == food:
-        #     break
+        optimalvalues.append(episode_rewards)
 
-    optimalvalues.append(sarsa.episode_reward)
+    moving_avg = moving_average(optimalvalues)
 
-
-moving_avg = np.convolve(optimalvalues, np.ones(
-    (SHOW_EVERY,))/SHOW_EVERY, mode='valid')
-
-plt.plot([i for i in range(len(moving_avg))], moving_avg)
-plt.ylabel(f"Reward {SHOW_EVERY}ma")
-plt.xlabel("episode #")
-plt.show()
+    fig, ax = plt.subplots(figsize=(10, 8))
+    ax.plot([i for i in range(len(optimalvalues))], optimalvalues)
+    ax.set_ylabel(f"Rewards")
+    ax.set_xlabel("Episodes")
+    ax.set_title(f'{sarsa_learning}')
+    fig.savefig(f'sarsa/{llambda}.png')
+    # plt.show()
--- sarsa/sarsa.py
+++ sarsa/sarsa.py
@@ -1,71 +1,73 @@
 import pickle
+
 import numpy as np
 
 
 class Sarsa():
-    def __init__(self, _gamma: float, _lambda: float, _epsilon: float, q_table: dict, nsa_table: dict, eligibility_traces: dict):
+    def __init__(self, _gamma: float, _lambda: float, _epsilon: float, q_table: dict, nsa_table: dict = {}, eligibility_traces: dict = {}, ns_table: dict = {}):
         self.gamma = _gamma
         self.llambda = _lambda
         self.epsilon = _epsilon
         self.q_table = q_table
         self.nsa_table = nsa_table
+        self.ns_table = ns_table
         self.eligibility_traces_initialize = eligibility_traces
+        self.rng = np.random.default_rng()
 
-        self.player = None
-        self.target = None
-        self.enemy = None
-
-        self.actions = None
+        self.episode_actions = None
         self.episode_states = None
         self.episode_reward = None
         self.alpha = None
         self.delta = None
         self.eligibility_traces = None
 
-    def load_nsa_table(self, path):
-        with open(path, "rb") as f:
-            self.nsa_table = pickle.load(f)
+    def __str__(self) -> str:
+        return f'Sarsa: $\gamma$ = {self.gamma} | $\lambda$ = {self.llambda} | $\epsilon$ = {self.epsilon}'
 
     def new_episode(self):
-        self.player = None
-        self.target = None
-        self.enemy = None
+        self.episode_actions = []  # episode actions
+        self.episode_states = []  # episode states
+        self.episode_reward = []  # episode rewards
 
-        self.actions = []
-        self.episode_states = []
-        self.episode_reward = []
-        self.alpha = 0
-        self.delta = 0
         self.eligibility_traces = self.eligibility_traces_initialize.copy()
 
-    def append_episode_state(self, state):
+    def update_episode(self, state, reward, action):
         self.episode_states.append(state)
-
-    def set_reward(self, reward):
         self.episode_reward.append(reward)
+        self.episode_actions.append(action)
+
+        self.nsa_table[state][action] += 1
+        self.ns_table[state] += 1
 
     def get_total_reward(self):
         return np.sum(self.episode_reward)
 
-    def update(self):
+    def update_tables(self):
         delta = self.episode_reward[-1] + self.gamma*self.q_table[self.episode_states[-1]
-                                                                  ][self.actions[-1]] - self.q_table[self.episode_states[-2]][self.actions[-2]]
+                                                                  ][self.episode_actions[-1]] - self.q_table[self.episode_states[-2]][self.episode_actions[-2]]
         self.eligibility_traces[self.episode_states[-2]] += 1
 
         for step, state in enumerate(self.episode_states):
-            alpha = 1/self.nsa_table[state][self.actions[step]]
-            self.q_table[self.episode_states[-2]][self.actions[-2]
-                                                  ] += alpha * delta * self.eligibility_traces[state][self.actions[step]]
+            alpha = 1/(self.nsa_table[state]
+                       [self.episode_actions[step]])
 
-            self.eligibility_traces[state][self.actions[step]
+            self.q_table[self.episode_states[-2]][self.episode_actions[-2]
+                                                  ] += alpha * delta * self.eligibility_traces[state][self.episode_actions[step]]
+
+            self.eligibility_traces[state][self.episode_actions[step]
                                            ] *= self.gamma * self.llambda
 
-    def set_nsa_table(self, state, action: int):
-        self.nsa_table[state][action] += 1
-
-    def get_action(self, state):
+    def epsilon_greedy_policy(self, state):
         max_value = np.max(self.q_table[state])
-        action = np.random.choice(
-            np.where(self.q_table[state] == max_value)[0])
-        self.actions.append(action)
+        opt_action = np.where(self.q_table[state] == max_value)[0]
+
+        # action = np.random.choice(opt_action)
+        if len(opt_action) > 1:
+            action = np.random.choice(opt_action)
+        else:
+            epsilon = self.epsilon/(self.epsilon + self.ns_table[state])
+            prob_actions = np.zeros(4) + epsilon/4
+            prob_actions[opt_action] += 1 - epsilon
+            action = self.rng.choice(4, p=prob_actions)
+
         return action
