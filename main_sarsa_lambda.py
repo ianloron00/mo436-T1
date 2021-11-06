984482e710a7c77b82e6fccc46084eb111e38735
Guilherme Oliveira <guilherme.oliveira.mec@gmail.com>
2021-11-06 11:29:15 -0300

    Treinamentos sarsa Lambda com gamma = 0.1

--- main_sarsa_lambda.py
+++ main_sarsa_lambda.py
@@ -9,16 +9,16 @@ from sarsa_lambda.basis.dependencies import *
 from sarsa_lambda.sarsa_lambda import SarsaLambda
 
 isTraining = True
-isStochastic = False
+isStochastic = True
 SAVE_IMAGES = True
 PATH = '.data'
 
-EPISODES = 100_000 if isTraining else 10
-GAMMA = 0.90 if isTraining else 0.0
-CONST_EPSILON = 10000 if isTraining else 0
-LAMBDA = 0
+EPISODES = 150_000 if isTraining else 10
+GAMMA = 0.10 if isTraining else 0.0
+CONST_EPSILON = [100, 1000, 10000] if isTraining else 0
+LAMBDA = [0, 0.2, 0.4, 0.6, 0.8, 1]
 
-SHOW_EVERY = int(EPISODES/200) if isTraining else 1
+SHOW_EVERY = int(EPISODES/20) if isTraining else 1
 
 NOW = datetime.now().strftime('%Y%m%d_%H%M%S')
 NAME_COMPLEMENT = '_stochastic' if isStochastic else '_deterministic'
@@ -41,80 +41,83 @@ start_ns_table = None if isTraining else NAME_NS_TABLE + '.pickle'
 start_nsa_table = None if isTraining else NAME_NSA_TABLE + '.pickle'
 
 def sarsa_lambda(env):
-    agent = SarsaLambda(gamma=GAMMA, const_epsilon=CONST_EPSILON, lambd = LAMBDA)
-    agent.initialize(env)
+    for lambd in LAMBDA:
+        for const_eps in CONST_EPSILON:
+            print(f"lambda  = {lambd} and N0 = {const_eps}")
+            agent = SarsaLambda(gamma=GAMMA, const_epsilon=const_eps, lambd = lambd)
+            agent.initialize(env)
 
-    scores = []
-    wins = []
-    time_fast = 5 if isTraining else 30
-    time_slow = 400
+            scores = []
+            wins = []
+            time_fast = 5 if isTraining else 30
+            time_slow = 400
 
-    for i in range(EPISODES):
-        obs = env.reset()
-        done = False
-        episode_reward = 0
+            for i in range(EPISODES):
+                obs = env.reset()
+                done = False
+                episode_reward = 0
 
-        if i % SHOW_EVERY == 0:
-            print(f"on # {i}, the known states are {agent.count_states()}")
-            if i != 0:
-                print(f"{SHOW_EVERY} ep mean {np.mean(scores[-SHOW_EVERY:])}")
-            show = False
-        else:
-            show = False
+                if i % SHOW_EVERY == 0:
+                    print(f"on # {i}, the known states are {agent.count_states()}")
+                    if i != 0:
+                        print(f"{SHOW_EVERY} ep mean {np.mean(scores[-SHOW_EVERY:])}")
+                    show = False
+                else:
+                    show = False
 
-        if isTraining:
-            action = agent.epsilon_greedy_policy(env, obs)
-        else:
-
-            action = agent.greedy_policy(env, obs)
-
-        while done == False:
-
-            new_obs, reward, done, _ = env.step(action)
-            episode_reward += reward
-
-            if not done:
                 if isTraining:
-                    new_action = agent.epsilon_greedy_policy(env, obs)
+                    action = agent.epsilon_greedy_policy(env, obs)
                 else:
 
-                    new_action = agent.greedy_policy(env, obs)
+                    action = agent.greedy_policy(env, obs)
 
-            agent.update(env, obs, action, new_obs, new_action, reward, done)
+                while done == False:
 
-            if show:
-                env.render(time_fast=time_fast, time_slow=time_slow)
+                    new_obs, reward, done, _ = env.step(action)
+                    episode_reward += reward
 
-            if done:
-                scores.append(episode_reward)
-                wins.append(env.won)
+                    if not done:
+                        if isTraining:
+                            new_action = agent.epsilon_greedy_policy(env, obs)
+                        else:
 
-            obs = new_obs
-            action = new_action
+                            new_action = agent.greedy_policy(env, obs)
 
-    if SAVE_IMAGES:
-        print("SAVING IMAGES")
+                    agent.update(env, obs, action, new_obs, new_action, reward, done)
 
-    reward_title = 'sarsa_lambda_rewards_10x10' + NAME_COMPLEMENT
-    if not isTraining:
-        reward_title += '_after_training'
-    moving_avg = np.convolve(scores, np.ones((SHOW_EVERY,)) / SHOW_EVERY, mode='valid')
-    plot_rewards(moving_avg, title=reward_title, save=SAVE_IMAGES)
+                    if show:
+                        env.render(time_fast=time_fast, time_slow=time_slow)
 
-    victories_title = 'sarsa_lambda_victories_10x10' + NAME_COMPLEMENT
-    if not isTraining:
-        victories_title += '_after_training'
-    victories_avg = np.convolve(wins, np.ones((SHOW_EVERY,)) / SHOW_EVERY, mode='valid')
-    plot_victories(victories_avg, title=victories_title, save=SAVE_IMAGES)
+                    if done:
+                        scores.append(episode_reward)
+                        wins.append(env.won)
 
-    if (SAVE_TABLE):
-        path = 'sarsa_lambda/q_tables/' + NAME_Q_TABLE + '.pickle'
-        path2 = 'sarsa_lambda/scores/' + NAME_SCORES + '.pickle'
-        print(f"SAVING {NAME_Q_TABLE}")
-        with open(path, "wb") as f:
-          pickle.dump(agent.q_table, f)
-        with open(path2, "wb") as f:
-          pickle.dump(scores, f)
+                    obs = new_obs
+                    action = new_action
+
+            if SAVE_IMAGES:
+                print("SAVING IMAGES")
+
+            reward_title = 'sarsa_lambda_rewards_10x10' + NAME_COMPLEMENT
+            if not isTraining:
+                reward_title += '_after_training'
+            moving_avg = np.convolve(scores, np.ones((SHOW_EVERY,)) / SHOW_EVERY, mode='valid')
+            plot_rewards(moving_avg, title=reward_title, lambd = lambd, N0 = const_eps, save=SAVE_IMAGES)
+
+            victories_title = 'sarsa_lambda_victories_10x10' + NAME_COMPLEMENT
+            if not isTraining:
+                victories_title += '_after_training'
+            victories_avg = np.convolve(wins, np.ones((SHOW_EVERY,)) / SHOW_EVERY, mode='valid')
+            plot_victories(victories_avg, title=victories_title, lambd = lambd, N0 = const_eps, save=SAVE_IMAGES)
+
+            if (SAVE_TABLE):
+                path = 'sarsa_lambda/q_tables/' + NAME_Q_TABLE + '_lambda_' + str(lambd) + '_NO_' + str(const_eps) + '.pickle'
+                path2 = 'sarsa_lambda/scores/' + NAME_SCORES + '_lambda_' + str(lambd) + '_NO_' + str(const_eps) + '.pickle'
+                print(f"SAVING {NAME_Q_TABLE}")
+                with open(path, "wb") as f:
+                  pickle.dump(agent.q_table, f)
+                with open(path2, "wb") as f:
+                  pickle.dump(scores, f)
 
     env.close()
 
--- sarsa_lambda/basis/graphs.py
+++ sarsa_lambda/basis/graphs.py
@@ -1,19 +1,21 @@
 from dependencies import *
 import os
 
-def plot_rewards(moving_avg, title='rewards', save=True):
+def plot_rewards(moving_avg, title='rewards',lambd = 0, N0 = 0, save=True):
   plt.plot([i for i in range(len(moving_avg))], moving_avg)
   plt.ylabel(f"reward")
   plt.xlabel('episode #')
+  plt.title(r'$\lambda = {}; N0 = {}$'.format(lambd, N0))
   if not os.path.isdir('./monte_carlo/images/'):
       os.makedirs('./monte_carlo/images/')
   if save:
-    plt.savefig('monte_carlo/images/'+ title +'.png')
-  plt.show()
+    plt.savefig('sarsa_lambda/images/'+ title + '_lambda_' + str(lambd) + '_NO_' + str(N0) +'.png')
+  plt.close()
 
-def plot_victories(victories_avg, title='victories_iterations', save=True):
+
+def plot_victories(victories_avg, title='victories_iterations',lambd = 0, N0 = 0, save=True):
   plt.plot([i for i in range(len(victories_avg))], victories_avg)
-  plt.title("victories per number iterations")
+  plt.title(r'victories per number iterations $\lambda = {}; N0 = {}$'.format(lambd, N0))
   if save:
-    plt.savefig('monte_carlo/images/' + title + '.png')
-  plt.show()
+    plt.savefig('sarsa_lambda/images/' + title + '_lambda_' + str(lambd) + '_NO_' + str(N0) +'.png')
+  plt.close()
