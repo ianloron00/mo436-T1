37646e156cb2dae8c5e02603f0936ab35b181ef3
rebecapadovani <rebeca.ederli@unesp.br>
2021-12-19 15:33:17 -0300

    organizacao arquivos a2c

--- a2c/a2c.py
+++ a2c/a2c.py
@@ -11,25 +11,17 @@ class ActorCritic(nn.Module):
 
         self.num_actions = num_actions
         self.critic_linear1 = nn.Linear(num_inputs, hidden_size)
-        self.critic_linear2 = nn.Linear(hidden_size, hidden_size)
-        self.critic_linear3 = nn.Linear(hidden_size, hidden_size)
-        self.critic_linear4 = nn.Linear(hidden_size, 1)
+        self.critic_linear2 = nn.Linear(hidden_size, 1)
 
         self.actor_linear1 = nn.Linear(num_inputs, hidden_size)
-        self.actor_linear2 = nn.Linear(hidden_size, hidden_size)
-        self.actor_linear3 = nn.Linear(hidden_size, hidden_size)
-        self.actor_linear4 = nn.Linear(hidden_size, num_actions)
+        self.actor_linear2 = nn.Linear(hidden_size, num_actions)
         
     def forward(self, state):
         state = Variable(torch.from_numpy(state).float().unsqueeze(0))
         value = F.relu(self.critic_linear1(state))
         value = self.critic_linear2(value)
-        value = self.critic_linear3(value)
-        value = self.critic_linear4(value)
         
         policy_dist = F.relu(self.actor_linear1(state))
-        policy_dist = self.actor_linear2(policy_dist)
-        policy_dist = self.actor_linear3(policy_dist)
-        policy_dist = F.softmax(self.actor_linear4(policy_dist), dim=1)
+        policy_dist = F.softmax(self.actor_linear2(policy_dist), dim=1)
     
         return value, policy_dist
